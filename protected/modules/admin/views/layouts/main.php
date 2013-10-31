732b386055f6c886d62ca709c0baf14c517ae513
yiqiang3344 <yiqiang3344@gmail.com>
2013-10-31 21:38:20 +0800

    添加UEditor富文本编辑器

--- protected/modules/admin/views/layouts/main.php
+++ protected/modules/admin/views/layouts/main.php
@@ -12,6 +12,7 @@
             var STIME=<?php echo Y::getTime();?>;
             var CTIME=new Date().getTime();
             var TEST_SERVER_FLAG=<?php echo YII_DEBUG;?>;
+            window.UEDITOR_HOME_URL=<?php echo json_encode($this->getAssetsUrl().'/ueditor/');?>;
         </script>
         <script type="text/javascript" src="<?php echo $this->url('js/helper.js');?>"></script>
     </head>
--- protected/modules/admin/views/main/index.php
+++ protected/modules/admin/views/main/index.php
@@ -1,3 +1,5 @@
+<script type="text/javascript" src="<?php echo $this->url('ueditor/ueditor.config.js');?>"></script>
+<script type="text/javascript" src="<?php echo $this->url('ueditor/ueditor.all.js');?>"></script>
 <script type="text/javascript">//template
     var template = Hogan.compile(<?php echo json_encode($this->template);?>);
 </script>
@@ -11,7 +13,19 @@
 
     function content_refresh(){
         var html = template.render(params);
+        html += '<div>';
+        html += '<textarea name="后台取值的key" id="myEditor" style="height:400px;width:500px;">这里写你的初始化内容</textarea>';
+        html += '<input type="submit" id="submit" value="提交"/>';
+        html += '</div>';
         $('.maincontent').html(html);
+
+        var editor = UE.getEditor('myEditor');
+        editor.ready(function(){
+            this.setContent('test');
+        });
+        $('#submit').click(function(){
+            console.log(editor.getContent());
+        });
     }
 </script>
 <script type="text/javascript">//php代码只能出现在这个脚本中
