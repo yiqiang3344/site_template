d397796972ec70e79065b07b0785eb5a9c114043
sidney <yiqiang3344@gmail.com>
2014-04-08 22:09:45 +0800

    mustache改名

--- protected/config/main.php
+++ protected/config/main.php
@@ -32,11 +32,13 @@ return array(
             'urlFormat'=>'path',
             'showScriptName'=>false,
             'urlSuffix' => '.html',//后缀 
+            /*
             'rules'=>array(
-                // '<controller:\w+>/<id:\d+>'=>'<controller>/view',
-                // '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-                // '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+                '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
             ),
+            */
         ),
         'cache'=>array(
             'class' => 'CDummyCache',
